1f16e51fa71fcd03024bc0b2250c70f91b4a9e84
duck <2uan20004@gmail.com>
2024-11-08 11:56:27 +0700

    merge branches

---
+++ filters/translateFilter.js
@@ -0,0 +1,13 @@
+const { sendToQueue, consumeFromQueue } = require('../utils/connection');
+const { translate } = require('../utils/translate');
+
+async function translateFilter() {
+    consumeFromQueue('translateQueue', async (text) => {
+        // console.log('text from translateQueue' + text);
+        const translatedText = await translate(text);
+        console.log('translated text ' + translatedText);
+        sendToQueue('pdfQueue', translatedText);
+    })
+}
+
+translateFilter();
---
+++ utils/connection.js
@@ -0,0 +1,35 @@
+const amqp = require('amqplib');
+require('dotenv').config();
+
+async function connectToChannel(queueName) {
+    const connection = await amqp.connect(process.env.RABBITMQ_IP);
+    // console.log(process.env.RABBITMQ_IP);
+    const channel = await connection.createChannel();
+    await channel.assertQueue(queueName);
+    return channel;
+}
+
+async function sendToQueue(queueName, message) {
+    const channel = await connectToChannel(queueName);
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)));
+}
+
+async function consumeFromQueue(queueName, onMessage) {
+    const channel = await connectToChannel(queueName);
+    channel.consume(queueName, (msg) => {
+        if (msg) {
+            const message = JSON.parse(msg.content.toString());
+            onMessage(message);
+            channel.ack(msg);
+        }
+    })
+}
+
+// connectToChannel('test')
+
+module.exports = {
+    sendToQueue,
+    consumeFromQueue
+}
+  
+  
